e798541569d96288b1f2911e8a6eb76cc78ef07c
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2023-11-26 14:58:15 +0800

    add useState to handle array of new list names
    add handleChange to update state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from "react";
-
+import {useState} from 'react'
 import useLocalStorageTodos from "./components/useLocalStorageTodos";
 import TodoList from "./components/todolist"
 
 // Created an array of list names to easily manage three working lists.
-const LIST_NAMES = [ 'Work', "Life", "Pleasure" ]
+
+
 
 function App() {
+  const [listNames, setListName]= useState([''])
   // Set LocalStorage to handle synchronization. State for todos is handled in useLocalStorage.
   const [todos, setTodos] = useLocalStorageTodos();
+
+  function handleChange(event){
+    const {name, value}=event.target 
+    setListName(prevListName =>{
+      return{
+        ...prevListName,
+        name
+      }
+    })
+  }
 
   return (
     <div className="App">
       <h1>Daily To-do's</h1>
+      <input
+        className='list-title'
+        placeholder='Enter Your List Name'
+        value={listNames}
+        type="text"
+        name="listName"
+        onChange= {handleChange}
+      ></input>
       {/* Map over LIST_NAMES and pass in the name of the list and props from localStorage. */}
       <div className="wrapper">
-        {LIST_NAMES.map((listName) => (
+        {listNames.map((listName) => (
           <TodoList
             key={listName}
             todos={todos}
